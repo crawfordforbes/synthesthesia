6291e497aac187471964ad485f27180228b645cb
crawfordforbes <crawford.forbes@gmail.com>
2015-07-01 14:24:47 -0400

    added scale choices

--- public/scripts.js
+++ public/scripts.js
@@ -13,13 +13,31 @@ var bass = {};
 var tenor = {};
 var alto = {};
 var soprano = {};
+ //notes placeholder
+ var bassNotes = [];
+ var tenorNotes = [];
+ var altoNotes = [];
+ var sopranoNotes = [];
 
-var notes = ['C2','C#2','D2','D#2','E2','F2','F#2','G2','G#2','A2','A#2','B2','C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3','C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7','C#7','D7','D#7','E7','F7','F#7','G7','G#7','A7','A#7','B7','C8'];
-var bassNotes = ['C2','C#2','D2','D#2','E2','F2','F#2','G2','G#2','A2','A#2','B2','C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3','C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5']
-var tenorNotes = ['C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6']
-var altoNotes = ['C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7']
-var sopranoNotes = ['C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7','C#7','D7','D#7','E7','F7','F#7','G7','G#7','A7','A#7','B7','C8']
+//scale placeholder
+var scale = ""
 
+var chromatic = ['C2','C#2','D2','D#2','E2','F2','F#2','G2','G#2','A2','A#2','B2','C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3','C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7','C#7','D7','D#7','E7','F7','F#7','G7','G#7','A7','A#7','B7','C8'];
+var ionian = ['C2','C2','D2','E2','F2','F2','G2','G2','A2', 'B2','C3','C3','D3','E3','F3','F3','G3','G3','A3','B3','C4','C4','D4','E4','F4','F4','G4','G4','A4','B4','C5','C5','D5','E5','F5','F5','G5','G5','A5','B5','C6','C6','D6','E6','F6','F6','G6','G6','A6','B6','C7','C7','D7','E7','F7','F7','G7','G7','A7','B7','C8']
+var dorian = ['C2','C2','D2','D#2','F2','F2','G2','G2','A2','A#2','C3','C3','D3','D#3','F3','F3','G3','G3','A3','A#3','C4','C4','D4','D#4','F4','F4','G4','G4','A4','A#4','C5','C5','D5','D#5','F5','F5','G5','G5','A5','A#5','C6','C6','D6','D#6','F6','F6','G6','G6','A6','A#6','C7','C7','D7','D#7','F7','F7','G7','G7','A7','A#7','C8']
+var phrygian = ['C2','C#2','E2','F2','F2','G2','G2','Ab2','A#2','C3','C#3','E3','F3','F3','G3','G3','Ab3','A#3','C4','C#4','E4','F4','F4','G4','G4','Ab4','A#4','C5','C#5','E5','F5','F5','G5','G5','Ab5','A#5','C6','C#6','E6','F6','F6','G6','G6','Ab6','A#6','C7','C#7','E7','F7','F7','G7','G7','Ab7','A#7','C8']
+var aeolian = ['C2','C2','D2','D#2','F2','F2','G2','G2','Ab2','Bb2','C3','C3','D3','D#3','F3','F3','G3','G3','Ab3','Bb3','C4','C4','D4','D#4','F4','F4','G4','G4','Ab4','Bb4','C5','C5','D5','D#5','F5','F5','G5','G5','Ab5','Bb5','C6','C6','D6','D#6','F6','F6','G6','G6','Ab6','Bb6','C7','C7','D7','D#7','F7','F7','G7','G7','Ab7','Bb7','C8']
+var harmonicMinor = ['C2','C2','D2','D#2','F2','F2','G2','G2','Ab2','B2','C3','C3','D3','D#3','F3','F3','G3','G3','Ab3','B3','C4','C4','D4','D#4','F4','F4','G4','G4','Ab4','B4','C5','C5','D5','D#5','F5','F5','G5','G5','Ab5','B5','C6','C6','D6','D#6','F6','F6','G6','G6','Ab6','B6','C7','C7','D7','D#7','F7','F7','G7','G7','Ab7','B7','C8']
+var wholeTone = ['C2','D2','E2','F#2','G#2','A#2','C3','D3','E3','F#3','G#3','A#3','C4','D4','E4','F#4','G#4','A#4','C5','D5','E5','F#5','G#5','A#5','C6','D6','E6','F#6','G#6','A#6','C7','D7','E7','F#7','G#7','A#7','C8']
+// var bassNotes = ['C2','C#2','D2','D#2','E2','F2','F#2','G2','G#2','A2','A#2','B2','C3','C#3','D3','D#3','E3','F3','F#3','G3','G#3','A3','A#3','B3','C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5']
+// var tenorNotes = ['C4','C#4','D4','D#4','E4','F4','F#4','G4','G#4','A4','A#4','B4','C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6']
+// var altoNotes = ['C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7']
+// var sopranoNotes = ['C5','C#5','D5','D#5','E5','F5','F#5','G5','G#5','A5','A#5','B5','C6','C#6','D6','D#6','E6','F6','F#6','G6','G#6','A6','A#6','B6','C7','C#7','D7','D#7','E7','F7','F#7','G7','G#7','A7','A#7','B7','C8']
+
+var bassNotes = ['C2','C2','D2','D2','E2','F2','F2','G2','G2','A2','A2','B2','C3','C3','D3','D3','E3','F3','F3','G3','G3','A3','A3','B3','C4','C4','D4','D4','E4','F4','F4','G4','G4','A4','A4','B4','C5']
+var tenorNotes = ['C4','C4','D4','D4','E4','F4','F4','G4','G4','A4','A4','B4','C5','C5','D5','D5','E5','F5','F5','G5','G5','A5','A5','B5','C6']
+var altoNotes = ['C5','C5','D5','D5','E5','F5','F5','G5','G5','A5','A5','B5','C6','C6','D6','D6','E6','F6','F6','G6','G6','A6','A6','B6','C7']
+var sopranoNotes = ['C5','C5','D5','D5','E5','F5','F5','G5','G5','A5','A5','B5','C6','C6','D6','D6','E6','F6','F6','G6','G6','A6','A6','B6','C7','C7','D7','D7','E7','F7','F7','G7','G7','A7','A7','B7','C8']
 var url = document.getElementById("url");
 var submit = document.getElementById("submit");
 submit.addEventListener("click", function(){
@@ -39,10 +57,35 @@ submit.addEventListener("click", function(){
 		var bodyClose = html.html.body.indexOf("</body>")
 		var body = html.html.body.slice(bodyOpen, bodyClose).replace(/[^a-zA-Z]/g, "")
 		console.log("body is " + body.length + " characters long")
-		instrumentMaker(head, body)
+		var scaleButtons = document.getElementsByName("tonality")
+
+		for(var i = 0; i < scaleButtons.length; i++){
+			if (scaleButtons[i].checked){
+				scaleName = scaleButtons[i].value
+				if (scaleName === "dorian"){
+					scale = dorian
+				} else if (scaleName === "phrygian"){
+					scale = phrygian
+				} else if (scaleName === "ionian"){
+					scale = ionian
+				} else if (scaleName === "aeolian"){
+					scale = aeolian
+				} else if (scaleName === "harmonicMinor"){
+					scale = harmonicMinor
+				} else if (scaleName === "wholeTone"){
+					scale = wholeTone
+				} else if (scaleName === "chromatic"){
+					scale = chromatic
+				} else {
+					console.log("scale does not exist")
+				}
+				console.log(scaleName);
+				instrumentMaker(head, body, scale)
+			}
+		}
 	})
-	var site = JSON.stringify({site: url.value})
-	xhr.send(site)
+var site = JSON.stringify({site: url.value})
+xhr.send(site)
 })
 
 var waveAssign = function(head) {
@@ -57,7 +100,7 @@ var waveAssign = function(head) {
 	}
 }
 
-var instrumentMaker = function(head, body) {
+var instrumentMaker = function(head, body, scale) {
 	wave1 = waveAssign(head);
 	wave2 = waveAssign(head);
 	wave3 = waveAssign(head);
@@ -102,46 +145,37 @@ var instrumentMaker = function(head, body) {
 			release: numbers.indexOf(head[23])/26
 		}
 	})
-	// soprano.play({pitch: notes[numbers.indexOf(head[24])]})
-	// alto.play({pitch: "C4"})
-	// tenor.play({pitch: "F#4"})
-	// bass.play({pitch: "D3"})
-	musicPlayer(soprano, alto, tenor, bass, body)
+
+	musicPlayer(soprano, alto, tenor, bass, body, scale)
 };
 
-var musicPlayer = function(soprano, alto, tenor, bass, body) {
+var musicPlayer = function(soprano, alto, tenor, bass, body, scale) {
 	var bassString = body.slice(0, body.length / 4)
 	var tenorString = body.slice(body.length / 4, body.length / 2)
 	var altoString = body.slice(body.length / 2, body.length / 4 * 3)
 	var sopranoString = body.slice(body.length / 4 * 3, body.length)
-	melodyMaker("bass", bassNotes, bass, bassString);
-	melodyMaker("tenor", tenorNotes, tenor, tenorString);
-	melodyMaker("alto", altoNotes, alto, altoString);
-	melodyMaker("soprano", sopranoNotes, soprano, sopranoString);
-	// for(var b = 0; b <= bassString.length; b+=2){
-	// 	console.log("bass is playing " + notes[numbers.indexOf(bassString[b])] + " after waiting " + numbers.indexOf(bassString[b+1]) + " seconds");
-	// 	bass.play({pitch: notes[numbers.indexOf(bassString[b])], wait: numbers.indexOf(bassString[b+1])})
-	// }
-	// 	for(var t = 0; t <= tenorString.length; t+=2){
-	// 	console.log("tenor is playing note " + notes[numbers.indexOf(tenorString[t])] + " after waiting " + numbers.indexOf(tenorString[t+1]) + " seconds");
-	// 	tenor.play({pitch: notes[numbers.indexOf(tenorString[t])], wait: numbers.indexOf(tenorString[t+1])})
-	// }
-	// 		for(var a = 0; a <= altoString.length; a+=2){
-	// 	console.log("alto is playing note " + notes[numbers.indexOf(altoString[a])] + " after waiting " + numbers.indexOf(altoString[a+1]) + " seconds");
-	// 	alto.play({pitch: notes[numbers.indexOf(altoString[a])], wait: numbers.indexOf(altoString[a+1])})
-	// }
-	// 	for(var s = 0; s <= sopranoString.length; s+=2){
-	
-	// 	console.log("soprano is playing note " + notes[Math.floor(numbers.indexOf(sopranoString[s]/2)) + 38] + " after waiting " + numbers.indexOf(sopranoString[s+1]) + " seconds");
-	// 	soprano.play({pitch: notes[Math.floor(numbers.indexOf(sopranoString[s]/2)) + 38], wait: numbers.indexOf(sopranoString[s+1])})
-	// }
-
+	melodyMaker("bass", scale, bass, bassString);
+	melodyMaker("tenor", scale, tenor, tenorString);
+	melodyMaker("alto", scale, alto, altoString);
+	melodyMaker("soprano", scale, soprano, sopranoString);
 }
 
-var melodyMaker = function(name, notes, voice, string){
+var melodyMaker = function(name, scaleChoice, voice, string){
+	if (name === "bass"){
+		voiceScale = scaleChoice.slice(0, Math.floor(scaleChoice.length * .4))
+		console.log(voiceScale)
+	} else if (name === "tenor"){
+		voiceScale = scaleChoice.slice(Math.floor(scaleChoice.length * .2), Math.floor(scaleChoice.length * .6))
+	} else if (name === "alto"){
+		voiceScale = scaleChoice.slice(Math.floor(scaleChoice.length * .4), Math.floor(scaleChoice.length * .8))
+	} else if (name === "soprano"){
+		voiceScale = scaleChoice.slice(Math.floor(scaleChoice.length * .4), scaleChoice.length)
+	} else {
+		console.log(name + " does not exist yet in melodyMaker")
+	}
 	for(var i = 0; i<100; i += 2){
-		var thisNote = notes[numbers.indexOf(string[i]) % notes.length];
-		var thisWait = numbers.indexOf(string[i+1]) + (i * 1.5);
+		var thisNote = voiceScale[numbers.indexOf(string[i]) % voiceScale.length];
+		var thisWait = numbers.indexOf(string[i+1]) + (i * .75);
 		console.log(name + " is playing note " + thisNote + " after waiting " + thisWait + " seconds.");
 		voice.play({pitch: thisNote, wait: thisWait})
 	}
